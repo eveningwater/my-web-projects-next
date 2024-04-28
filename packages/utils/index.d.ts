bc20c662bf8bc8cc9ac0d5377d31b45c4a5fe17c
eveningwater <854806732@qq.com>
2024-04-28 22:52:59 +0800

    feat: 添加了一些示例

--- packages/utils/index.d.ts
+++ packages/utils/index.d.ts
@@ -1,5 +1,13 @@
+type EventElement = HTMLElement | Document | Window;
 interface Utils {
-    isServer: boolean
+    isServer: boolean;
+    eventType: string[];
+    isMobile: () => boolean;
+    on: (e: EventElement, t: string, c: EventListenerOrEventListenerObject, u: boolean) => void;
+    off: (e: EventElement, t: string, c: EventListenerOrEventListenerObject, u: boolean) => void;
+    baseClickOutSide: (e: HTMLElement, b: boolean, c: (arg0: EventTarget, arg1: DOMRect) => void) => void;
+    isFunction: <T>(v: T) => boolean;
+    getRect: (e: HTMLElement | Range) => DOMRect;
 }
-declare const ewUtils: Utils;
+declare const ewUtils: () => Utils;
 export default ewUtils;
--- packages/utils/index.js
+++ packages/utils/index.js
@@ -1,5 +1,57 @@
-const ewUtils = {
-  isServer: typeof window === "undefined"
-}
+const ewUtils = function () {
+  if (new.target !== ewUtils) {
+    throw new Error('error');
+  }
+  this.eventType = this.isMobile()
+    ? ['touchstart', 'touchmove', 'touchend']
+    : ['mousedown', 'mousemove', 'mouseup'];
+};
+
+ewUtils.prototype = {
+  constructor: ewUtils,
+  isMobile() {
+    return !!navigator.userAgent.match(/(iPhone|iPod|Android|ios)/i);
+  },
+  isServer: typeof window === 'undefined',
+  on(element, type, handler, useCapture = false) {
+    if (element && type && handler) {
+      element.addEventListener(type, handler, useCapture);
+    }
+  },
+  off(element, type, handler, useCapture = false) {
+    if (element && type && handler) {
+      element.removeEventListener(type, handler, useCapture);
+    }
+  },
+  baseClickOutSide(element, isUnbind = true, callback) {
+    const mouseHandler = event => {
+      const target = event.target;
+      if (!target) return;
+      const targetRect = this.getRect(target);
+      const rect = this.getRect(element);
+      if (
+        targetRect.x >= rect.x &&
+        targetRect.y >= rect.y &&
+        targetRect.width <= rect.width &&
+        targetRect.height <= rect.height
+      )
+        return;
+      if (this.isFunction(callback)) callback(target, targetRect);
+      if (isUnbind) {
+        // 延迟解除绑定
+        setTimeout(() => {
+          this.off(document, this.eventType[0], mouseHandler);
+        }, 0);
+      }
+    };
+    this.on(document, this.eventType[0], mouseHandler);
+  },
+  isFunction(v) {
+    return typeof v === 'function';
+  },
+  getRect(element) {
+    return element.getBoundingClientRect();
+  }
+};
 
 export default ewUtils;
