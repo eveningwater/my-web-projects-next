912ee3323e6344673c44964b348ae25a0304064d
eveningwater.123 <eveningwater.123@bytedance.com>
2025-05-06 14:12:11 +0800

    feat: 优化了代码

--- packages/projects/react/4/src/App.tsx
+++ packages/projects/react/4/src/App.tsx
@@ -1,16 +1,14 @@
-import ewMessage from 'ew-message';
-import ewUtils from 'ew-utils';
-import React from 'react';
-import Lists, { TodoListItem } from './components/Lists';
-import AddEditButton from './components/AddEditButton';
-import TodoInput from './components/TodoInput';
-
-const utils = new ewUtils();
+import ewMessage from "ew-message";
+import utils from "ew-utils";
+import React from "react";
+import Lists, { TodoListItem } from "./components/Lists";
+import AddEditButton from "./components/AddEditButton";
+import TodoInput from "./components/TodoInput";
 export interface AppProps {
   className?: string;
 }
 const App: React.FC<AppProps> = ({ className }) => {
-  const [state, setState] = React.useState(''); //输入的值
+  const [state, setState] = React.useState(""); //输入的值
   const [editUUid, setEditUUid] = React.useState<string | number>(-1); //编辑事项的uuid
   const [listData, setListData] = React.useState<TodoListItem[]>([]); //todoList
   // 请求数据
@@ -18,7 +16,7 @@ const App: React.FC<AppProps> = ({ className }) => {
     let todoList: TodoListItem[] = [];
     todoList.push({
       uuid: utils.createUUID(),
-      text: 'Now you can learn react.js by writing the todo-list application!'
+      text: "Now you can learn react.js by writing the todo-list application!",
     });
     setListData(todoList);
   };
@@ -28,23 +26,23 @@ const App: React.FC<AppProps> = ({ className }) => {
   };
   // 点击确认添加或确认编辑
   const handleToDo = (editUUid: string | number) => {
-    if (!state) return ewMessage.error('请输入需要待办的事项!');
+    if (!state) return ewMessage.error("请输入需要待办的事项!");
     // 等于-1表示是添加否则是编辑
     if (editUUid === -1) {
       let newTodoList: TodoListItem[] = [];
       newTodoList.push({
         text: state,
-        uuid: utils.createUUID()
+        uuid: utils.createUUID(),
       });
       setListData(listData.concat(newTodoList));
-      setState('');
+      setState("");
     } else {
-      let idx = listData.findIndex(_ => _.uuid === editUUid);
+      let idx = listData.findIndex((_) => _.uuid === editUUid);
       if (idx > -1) {
         listData[idx].text = state;
       }
       setListData(listData);
-      setState('');
+      setState("");
       setEditUUid(-1);
     }
   };
@@ -66,24 +64,24 @@ const App: React.FC<AppProps> = ({ className }) => {
       setState(item.text!);
     } else {
       setEditUUid(-1);
-      setState('');
+      setState("");
     }
   };
   // 点击删除
   const handleDelete = (item: Partial<TodoListItem>) => {
     window.ewConfirm({
-      title: '温馨提示',
-      content: '确定要删除该待办事项吗?',
+      title: "温馨提示",
+      content: "确定要删除该待办事项吗?",
       showCancel: true,
       sure: (context: { close: () => void }) => {
         context.close();
         // copy the listData and splice the item,set the copyable data to listData,the dom can update the listData
         // so why?
         const data = listData.slice();
-        const idx = data.findIndex(_ => _.uuid === item.uuid);
+        const idx = data.findIndex((_) => _.uuid === item.uuid);
         if (idx > -1) data.splice(idx, 1);
         setListData(data);
-      }
+      },
     });
   };
   // 点击查看待办事项详情
@@ -91,7 +89,7 @@ const App: React.FC<AppProps> = ({ className }) => {
     return ewMessage.info({
       showClose: true,
       content: `待办事项详情:${item.uuid}:${item.text}`,
-      duration: 0
+      duration: 0,
     });
   };
   React.useEffect(() => {
@@ -101,9 +99,9 @@ const App: React.FC<AppProps> = ({ className }) => {
     <section className={className}>
       <div className="handle-container">
         <TodoInput
-          handleChange={e => handleChange(e)}
+          handleChange={(e) => handleChange(e)}
           todo_value={state}
-          handleKeyDown={e => handleKeyDown(e)}
+          handleKeyDown={(e) => handleKeyDown(e)}
         />
         <AddEditButton
           onToDo={() => handleToDo(editUUid)}
@@ -111,10 +109,10 @@ const App: React.FC<AppProps> = ({ className }) => {
         />
       </div>
       <Lists
-        handleEdit={item => handleEdit(item, editUUid)}
+        handleEdit={(item) => handleEdit(item, editUUid)}
         todoList={listData}
-        handleDelete={item => handleDelete(item)}
-        seeDetail={item => seeDetail(item)}
+        handleDelete={(item) => handleDelete(item)}
+        seeDetail={(item) => seeDetail(item)}
         editUUid={editUUid}
       />
     </section>
